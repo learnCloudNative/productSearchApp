39bef77e7962e47bcd805e52d677b32e929939b7
Swarajit Roy <swarajit.roy@maersk.com>
2019-12-01 15:24:45 +0530

    template application

---
+++ src/main/java/net/containercrush/enceladus/productsearchapp/search/ProductSearchRestController.java
@@ -0,0 +1,36 @@
+package net.containercrush.enceladus.productsearchapp.search;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.containercrush.enceladus.productsearchapp.model.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class ProductSearchRestController {
+
+    @GetMapping(path = "/hello", produces=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> sayHello()
+    {
+         //Get data from service layer into entityList.
+
+        List<Product> entities = new ArrayList<Product>();
+        Product product = new Product() ;
+        product.setName("Zodiac Linen Blue");
+        entities.add(product);
+        
+        return new ResponseEntity<Object>(entities, HttpStatus.OK);
+    }
+
+
+}
+
+
